1a9ec63eb22f711014145727b495842b1318beb7
chinthomas <chengyou928@gmail.com>
2023-12-14 20:20:43 +0800

    fix: card table add items

--- final_project/src/db/schema.ts
+++ final_project/src/db/schema.ts
@@ -3,7 +3,9 @@ import { date, index, pgTable, serial, uuid, varchar } from "drizzle-orm/pg-core
 export const cardsTable = pgTable("crads",{
     id:serial("id").primaryKey(),
     displayId:uuid("display_id").defaultRandom().notNull().unique(),
+    title: varchar("title",{length:50}).notNull(),
     content: varchar("content",{length: 300}).notNull(),
+    imageUrl: varchar("image_url").default("/1280.jpeg"),
     date:date("date").defaultNow(),
 },
 (table) => ({
--- final_project/src/lib/types/db.ts
+++ final_project/src/lib/types/db.ts
@@ -1,5 +1,7 @@
 export type Card = {
     id: string;
+    title: string;
     content: string;
+    imageUrl?:string;
     date: string;
 }
