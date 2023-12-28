68bbd5e19ccff59caf3cf9812d2620793754383a
chinthomas <chengyou928@gmail.com>
2023-12-28 22:38:49 +0800

    fix: type of database item

--- final_project/src/lib/types/db.ts
+++ final_project/src/lib/types/db.ts
@@ -6,6 +6,13 @@ export type CardItem = {
     date: string;
 }
 
+export type WritingItem = {
+    id: string;
+    rowContent: string;
+    unemotionalContent: string;
+    writer?: string;
+}
+
 export type Message = {
     role: "user"| "system"| "assistant";
     content: string| null;
