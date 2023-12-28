d9e0b20b29cf89f2c9daff5228b0e48f02537332
AndyChiu <andychiu609@gmail.com>
2023-12-28 23:10:38 +0800

    some ui coding fix

--- final_project/src/app/critic/[cardId]/page.tsx
+++ final_project/src/app/critic/[cardId]/page.tsx
@@ -3,36 +3,10 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { Typography, Button, Card, CardContent, Box } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 import { useCard } from '@/hooks/useCard';
 import Form from './_component/Form';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    padding: theme.spacing(2.5),
-  },
-  backButton: {
-    marginTop: theme.spacing(2),
-    backgroundColor: theme.palette.primary.main,
-    '&:hover': {
-      backgroundColor: theme.palette.primary.dark,
-    },
-    color: theme.palette.common.white,
-    fontWeight: theme.typography.fontWeightBold,
-    padding: theme.spacing(1, 2),
-    borderRadius: theme.shape.borderRadius,
-    transition: theme.transitions.create(['background-color', 'box-shadow']),
-  },
-  commentSection: {
-    marginTop: theme.spacing(2),
-  },
-  commentCard: {
-    marginBottom: theme.spacing(2),
-  },
-}));
-
 function CriticPage() {
-  const classes = useStyles();
   const { cardItem } = useCard();
   const [comments, setComments] = useState([]);
 
@@ -50,7 +24,7 @@ function CriticPage() {
   }
 
   return (
-    <Box className={classes.root}>
+    <Box style={{ padding: '20px' }}>
       <Typography variant="h4" component="h1" gutterBottom>
         {cardItem.title}
       </Typography>
@@ -58,16 +32,16 @@ function CriticPage() {
         {cardItem.description}
       </Typography>
       <Link href="/" passHref>
-        <Button variant="contained" className={classes.backButton}>
+        <Button variant="contained" style={{ marginTop: '16px', backgroundColor: '#3f51b5', color: 'white' }}>
           Back to Home
         </Button>
       </Link>
       <Form />
 
-      <Box className={classes.commentSection}>
+      <Box style={{ marginTop: '16px' }}>
         <Typography variant="h5">評論</Typography>
         {comments.map((comment, index) => (
-          <Card key={index} className={classes.commentCard}>
+          <Card key={index} style={{ marginBottom: '8px' }}>
             <CardContent>
               <Typography variant="body1">{comment.content}</Typography>
             </CardContent>
