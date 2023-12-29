1c8e97955d05938c68c36e98ea9485f0d0811372
chinthomas <chengyou928@gmail.com>
2023-12-29 17:25:05 +0800

    main page header

--- final_project/src/app/layout.tsx
+++ final_project/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Header from '@/components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Header/>
       <body className={inter.className}>{children}</body>
     </html>
   )
